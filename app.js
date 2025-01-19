bdf81618bbb2382a714dcc46b3216a74578303e3
fg70226 <fg70226@ubt-uni.net>
2025-01-18 23:11:40 -0800

    responsive

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 
+function toggleMenu() {
+    const menu = document.querySelector('.menu-nav ul');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block'; 
+    } else {
+        menu.style.display = 'none'; 
+    }
+}
+
+document.addEventListener('click', function (event) {
+    const menu = document.querySelector('.menu-nav ul');
+    const toggleButton = document.querySelector('.menu-toggle');
+
+    
+    if (!menu.contains(event.target) && !toggleButton.contains(event.target)) {
+        menu.style.display = 'none'; 
+    }
+});
+
