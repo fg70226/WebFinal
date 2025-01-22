4e57703de5c2fc94715ec620d5ef64d696355a96
ru70129 <ru70129@ubt-uni.net>
2025-01-22 17:35:39 +0100

    Slider

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+var i=0;
+var imgArray = [
+    "D&Glogo.jpg",
+    "pradalogo.jpg",
+    "polologo.jpg",
+    "guccilogo.jpg",
+]
+var slideshow = document.getElementById("slideshow");
+function goToSlide(index){
+    slideshow.src = imgArray[index];
+}
